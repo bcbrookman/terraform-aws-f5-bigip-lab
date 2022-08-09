c5c65ce607a85d4a01b473f70dbe4ea522fde316
Brian Brookman <bcbrookman@users.noreply.github.com>
2022-08-09 08:34:25 -0700

    Fix issue with public IP assignment on BIG IP instance.
    Attaching the second internal interface after the instance is
    launched fixes the issue.

--- bigip.tf
+++ bigip.tf
@@ -1,4 +1,3 @@
-
 resource "aws_network_interface" "f5lab_bigip_external_if" {
   subnet_id   = aws_subnet.f5lab_external_net.id
   tags = { Name = "f5lab_bigip_external_if" }
@@ -12,14 +11,15 @@ resource "aws_network_interface" "f5lab_bigip_internal_if" {
 resource "aws_instance" "f5lab_bigip" {
   ami = "ami-0daffdedb72c05d5f"
   instance_type = "m4.large"
-  network_interface {
-      network_interface_id = aws_network_interface.f5lab_bigip_external_if.id
-      device_index         = 0
-  }
-  network_interface {
-      network_interface_id = aws_network_interface.f5lab_bigip_internal_if.id
-      device_index         = 1
-  }
   tags = { Name = "f5lab_bigip" }
-  depends_on = [ aws_subnet.f5lab_external_net ]
+  network_interface {
+    network_interface_id = aws_network_interface.f5lab_bigip_external_if.id
+    device_index         = 0
+  }
+}
+
+resource "aws_network_interface_attachment" "f5lab_bigip_internal_if_attachment" {
+  instance_id          = aws_instance.f5lab_bigip.id
+  network_interface_id = aws_network_interface.f5lab_bigip_internal_if.id
+  device_index         = 1
 }
